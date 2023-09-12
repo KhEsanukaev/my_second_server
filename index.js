4eda451046bfd593612ccc3fe4c31b941eb6d827
KhEsanukaev <halid.esanukaev.86@mail.ru>
2023-09-12 15:38:58 +0300

    dff

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Khalid!')
+app.get('/contacts', (req, res) => {
+  res.json({
+    city: "Grozny",
+    street: "Trosheva",
+    building: 7
+  })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
